2915c83f9112c59ddfa3992cd75ecd5457bd2c19
9bt <kohei_kubota@voyagegroup.com>
2020-05-12 23:58:18 +0900

    Enable to specify excluding file paths

--- main.php
+++ main.php
@@ -9,26 +9,31 @@ if ( ! isset( $argv[1] ) ) {
 $base_target_dir = $argv[1];
 $condition       = isset( $argv[2] ) ? $argv[2] : 'now';
 
-$target_paths = [];
-foreach ( glob( __DIR__ . '/resources/*.csv' ) as $resource_path ) {
-    $file = new SplFileObject( $resource_path );
-    $file->setFlags( SplFileObject::READ_CSV );
+$target_file = new SplFileObject( __DIR__ . '/resources/target.csv' );
+$target_file->setFlags( SplFileObject::READ_CSV );
 
-    foreach ( $file as $row ) {
-        $target_path = trim( $row[0] );
-        if ( '' === $target_path ) {
-            continue;
-        }
+$excluding_path_file = new SplFileObject( __DIR__ . '/resources/exclude.csv' );
+$excluding_path_file->setFlags( SplFileObject::READ_CSV );
 
-        $target_path = $base_target_dir . preg_replace( '/\.html$/', '.md', $target_path );
-        if ( ! file_exists( $target_path ) ) {
-            trigger_error( sprintf( '%s does not exist', $target_path ) , E_USER_WARNING );
-            continue;
-        }
+$excluding_paths = [];
+foreach( $excluding_path_file as $row ) {
+    $excluding_paths[] = trim( $row[0] );
+}
 
-        $content = file_get_contents( $target_path );
-        $content = UpdatedAtHandler::update_updated_at( $content, $condition );
-
-        file_put_contents( $target_path, $content );
+foreach ( $target_file as $row ) {
+    $target_path = trim( $row[0] );
+    if ( '' === $target_path || in_array( $target_path, $excluding_paths ) ) {
+        continue;
     }
+
+    $target_path = $base_target_dir . preg_replace( '/\.html$/', '.md', $target_path );
+    if ( ! file_exists( $target_path ) ) {
+        trigger_error( sprintf( '%s does not exist', $target_path ) , E_USER_WARNING );
+        continue;
+    }
+
+    $content = file_get_contents( $target_path );
+    $content = UpdatedAtHandler::update_updated_at( $content, $condition );
+
+    file_put_contents( $target_path, $content );
 }
